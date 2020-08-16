60185c46d7bab0a23061896ae59021026574798c
Carlos Germano <germano.carlos2712@gmail.com>
2020-08-16 16:33:24 -0300

    Updates

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "username": "admin",
-    "password": "Wanted1232100",
+    "username": "",
+    "password": "",
     "database": "league_dash",
     "host": "awsinstance.cajpsxy3wima.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
