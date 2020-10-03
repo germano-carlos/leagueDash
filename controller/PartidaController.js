a6c4244732e68cfad882c58825bd969399372bd8
Carlos Germano <germano.carlos2712@gmail.com>
2020-10-03 16:12:02 -0300

    [Feature] : Imagens dinamicas por personagem

--- controller/PartidaController.js
+++ controller/PartidaController.js
@@ -1,10 +1,12 @@
-/*const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
-const Partida = require('../app/models/Partida.js');*/
+/*const Partida = require('../app/models/Partida.js');*/
+
+const Campeao = require('../app/models/Campeao');
 const Api = require('../lib/Api');
 
-/*const connection = new Sequelize(dbConfig);
-Partida.init(connection);*/
+const connection = new Sequelize(dbConfig);
+Campeao.init(connection);
 
 module.exports = {
 
@@ -25,6 +27,7 @@ module.exports = {
                     let lane = element.lane;
                     let gameMode = Detalhes.data.gameMode;
                     let gameType = Detalhes.data.gameType;
+
                     let championId = 0;
                     let win = 0;
                     let kills = 0;
@@ -33,6 +36,8 @@ module.exports = {
                     
                     let participantId = 0;
                     let achou = false;
+                    let urlImage = "";
+
                     for(i=0;i<Detalhes.data.participantIdentities.length && !achou; i++)
                     {
                         if(Detalhes.data.participantIdentities[i].player.currentAccountId != jsonParams.account_id)
@@ -53,9 +58,12 @@ module.exports = {
                         deaths = Detalhes.data.participants[i].stats.deaths;
                         assists = Detalhes.data.participants[i].stats.assists;
                         achou = true;
+
+                        let champion = await Campeao.findOne({where: {key : championId}});
+                        urlImage = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.nome.replace(" ","")}_0.jpg`;
                     }
     
-                    PartidasDetalhes.push({lane, gameMode, gameType, championId, win, kills,deaths, assists});
+                    PartidasDetalhes.push({lane, gameMode, gameType, championId, win, kills,deaths, assists, urlImage});
                     if(PartidasDetalhes.length == Partidas.length || contador == 9)
                     {
                         resolve(PartidasDetalhes)
